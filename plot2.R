fc1617aa47891a3d695131e1c95c73339ad93f1b
heytitle <pat.chormai@gmail.com>
2016-01-25 03:03:20 +0100

    Commit

--- plot2.R
+++ plot2.R
@@ -3,13 +3,8 @@ data = read.csv("household_power_consumption.txt", sep=";",  stringsAsFactors=FA
 sub = data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
+filename = "plot2.png"
 
-filename = "plot1.png"
 png(filename, width=480, height=480 )
-hist(
-    as.numeric(sub$Global_active_power),
-    col="red",
-    xlab="Global Active Power (kilowatts)",
-    main="Global Active Power"
-)
+plot( datetime, as.numeric(sub$Global_active_power), type="l", ylab = "Global Active Power (kilowatts)", xlab="" )
 dev.off()
---
+++ plot3.R
@@ -0,0 +1,26 @@
+data = read.csv("household_power_consumption.txt", sep=";",  stringsAsFactors=FALSE)
+
+sub = data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+filename = "plot3.png"
+
+png(filename, width=480, height=480 )
+
+
+m1 = as.numeric(sub$Sub_metering_1)
+m2 = as.numeric(sub$Sub_metering_2)
+m3 = as.numeric(sub$Sub_metering_3)
+
+    plot( datetime, m1, type="l", ylab = "Energy sub metering", xlab="" )
+    lines( datetime, m2, type="l", ylab = "Energy sub metering", xlab="", col="red" )
+    lines( datetime, m3, type="l", ylab = "Energy sub metering", xlab="", col="blue" )
+
+
+    legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
+           col = c("black", "red","blue"),
+           lty = 1, lwd=2.5
+           )
+
+
+dev.off()
